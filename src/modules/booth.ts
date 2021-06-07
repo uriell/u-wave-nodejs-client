bc785a37705aece5e3019435a1be969a737d236f
Uriell <me+github@uriell.dev>
2021-06-06 23:05:05 -0300

    finishing up the vote method

--- src/modules/booth.ts
+++ src/modules/booth.ts
@@ -3,10 +3,12 @@ import { uWave } from '..';
 import { parseDates } from '../helpers';
 import { uWaveAPI } from '../types';
 import Auth from './auth';
-import type { HistoryEntry, Media, User } from '../types/entities';
+import { HistoryEntry, Media, User, VOTE_DIRECTIONS } from '../types/entities';
 
 export default class Booth {
   private uw: uWave;
+
+  static VOTE_DIRECTIONS = VOTE_DIRECTIONS;
 
   constructor(uw: uWave) {
     this.uw = uw;
@@ -53,5 +55,17 @@ export default class Booth {
     return this.uw
       .get<{}, uWaveAPI.CurrentVoteResponse>(`/booth/${historyId}/vote`)
       .then((res) => res.data);
+  }
+
+  public vote(
+    historyId: string,
+    direction: VOTE_DIRECTIONS.UPVOTE | VOTE_DIRECTIONS.DOWNVOTE
+  ) {
+    return this.uw
+      .put<uWaveAPI.VoteBody, uWaveAPI.VoteResponse>(
+        `/booth/${historyId}/vote`,
+        { direction }
+      )
+      .then(() => null);
   }
 }
