d12f897e20c98d8f6c64b81f7e38ae2ddeb8d290
Uriell <me+github@uriell.dev>
2021-07-03 19:07:56 -0300

    style(socket): renaming private token ref for reusability

--- src/modules/socket.spec.ts
+++ src/modules/socket.spec.ts
@@ -1,10 +1,10 @@
 import EventEmitter = require('events');
-import { PrivateSocketTokenRef, uWave } from '..';
+import { PrivateTokenRef, uWave } from '..';
 import SocketModule from './socket';
 
 test('should create an socket module instance', () => {
   const uw = new (class Test {})() as uWave;
-  const privateSocketTokenRef: PrivateSocketTokenRef = {};
+  const privateSocketTokenRef: PrivateTokenRef = {};
   const socket = new SocketModule(uw, privateSocketTokenRef);
 
   expect(socket).toBeInstanceOf(SocketModule);
